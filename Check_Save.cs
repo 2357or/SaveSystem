b87dd90f9ccc3ced931ddc6112f0626a637a9d0c
大槻亮太 <ootsukiryouta@MacBook-Pro-13-inch-2018.local>
2020-04-02 12:16:37 +0900

    ファイルを追加

---
+++ Check_Save.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[System.Serializable]   //[Serializable] をつけないとシリアライズできない
+public struct SaveData
+{
+    public int x;
+    public string y;
+    public bool z;
+
+}
+
+
+public class Check_Save : MonoBehaviour
+{
+    //保存するファイルのパス
+    private string PATH;
+
+    void Start() {
+        //SAVE_FILE_NAMEは　"hoge.json"　のようなファイル名
+        PATH = Application.dataPath + "/SaveData/" + "saveTest";
+    }
+
+    //セーブしたいクラス or 構造体　をインスタンス化
+    SaveData saveData = new SaveData();
+
+    void Update() {
+        //Sキーをおしてセーブ    
+        if(Input.GetKeyDown(KeyCode.S)) {
+            saveData.x = 10;
+            saveData.y = "test";
+            saveData.z = true;
+
+            SaveSystem.Save(saveData, PATH);
+
+            //Lキーをおしてロード
+        } else if(Input.GetKeyDown(KeyCode.L)) {
+            var Data
+              = JsonUtility.FromJson<SaveData>(SaveSystem.Load(PATH));
+
+            Debug.Log(Data.x);    //10
+            Debug.Log(Data.y);    //test
+            Debug.Log(Data.z);    //true
+        }
+    }
+}
---
+++ SaveSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class SaveSystem
+{
+   //セーブ
+   public static void Save(in object instance, string PATH, bool overWrite = false) {
+       string jsonstr = JsonUtility.ToJson(instance);
+       var writer = new StreamWriter(PATH, overWrite);
+       writer.Write(jsonstr);
+       writer.Flush();
+       writer.Close();
+       Debug.Log("Saved!");
+   }
+
+   //ロード
+   public static string Load(string PATH) {
+       var reader = new StreamReader(PATH);
+       string json = reader.ReadToEnd();
+       reader.Close();
+       Debug.Log("Loaded!");
+       return json;
+   }
+}
